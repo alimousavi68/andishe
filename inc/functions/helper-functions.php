50da8e8a189c0685e5daea6801b1cbc713f90817
Ali Mousavi <38689441+alimousavi68@users.noreply.github.com>
2024-04-22 13:22:04 +0330

    for test disable metaboxs

--- inc/functions/helper-functions.php
+++ inc/functions/helper-functions.php
@@ -121,7 +121,7 @@ add_action('save_post', 'save_post_subtitle');
  */
 
 // اضافه کردن یک متا باکس جدید به صفحه ایجاد و ویرایش پست ها
-add_action('add_meta_boxes', 'add_primary_category_meta_box');
+// add_action('add_meta_boxes', 'add_primary_category_meta_box');
 function add_primary_category_meta_box()
 {
     add_meta_box('primary_category_meta_box', 'داشبورد ویژه', 'render_primary_category_meta_box', 'post', 'side', 'high');
@@ -257,7 +257,7 @@ function render_primary_category_meta_box($post)
  * Save Meta box Values in Datatbase
  * 
  */
-add_action('save_post', 'save_primary_category_meta_data');
+// add_action('save_post', 'save_primary_category_meta_data');
 function save_primary_category_meta_data($post_id)
 {
     if (isset($_POST['hasht_primary_category'])) {
