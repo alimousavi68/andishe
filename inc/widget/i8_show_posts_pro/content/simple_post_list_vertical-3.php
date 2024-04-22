56e8026d1fd1737d75a0b1928cb29ef2fd9355b8
Ali Mousavi <38689441+alimousavi68@users.noreply.github.com>
2024-04-22 17:14:10 +0330

    fix new widget

--- inc/widget/i8_show_posts_pro/content/simple_post_list_vertical-3.php
+++ inc/widget/i8_show_posts_pro/content/simple_post_list_vertical-3.php
@@ -28,7 +28,7 @@ echo $sub_title_print . '</div>';
       <div
         class="col-12 col-xl-6 col-md-6 col-sm-12 vitrin-2-small-item1 position-relative px-1 px-xl-2 px-lg-2 px-sm-2 d-flex flex-column row-gap-2">
         <a href="<?php the_permalink(); ?>" class="image_frame">
-          <?php echo i8_the_thumbnail('i8-294-184', 'w-100 h-auto round-15px object-fit-cover overflow-hidden hover i8-img-fit ', $dimenition = array('width' => 294, 'height' => 196), true, '', false, true); ?>
+          <?php echo i8_the_thumbnail('i8-lg-290-163', 'w-100 h-auto  object-fit-cover overflow-hidden hover i8-img-fit ', $dimenition = array('width' => $thumb_width, 'height' => $thumb_height) , true, '', false, true); ?>
         </a>
         <div class="p-1">
           <span class="display-6">
